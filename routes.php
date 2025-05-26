ddd68ac6fdd7777ac98d65cd85e7d65d1057c47b
Georgi Angelov <georgi@mconverter.eu>
2025-05-26 19:40:36 +0300

    _

--- routes.php
+++ routes.php
@@ -4,41 +4,60 @@ use App\Controller\PostController;
 use App\Controller\UserController;
 
 $routes = [
-    // HTML API: GET /posts
-    // HTML API: GET /posts/<alias>||<id>
-    // HTML API: GET /post/<alias>||<id>
+    // =============================
+    // POSTS ROUTES
+    // =============================
 
-    new \PhpDevCommunity\Route('posts', '/posts', [PostController::class, 'index'], ['GET', 'HEAD']),
-    new \PhpDevCommunity\Route('posts.create_form', '/posts/create', [PostController::class, 'createForm'], ['GET']),
-    new \PhpDevCommunity\Route('posts.favorites', '/posts/favorites', [PostController::class, 'favoritesPage'], ['GET']),
-    new \PhpDevCommunity\Route('posts.comment', '/posts/favorites/{identifier}', [PostController::class, 'favoritesPage'], ['GET', 'HEAD']),
-    new \PhpDevCommunity\Route('posts.identifier', '/posts/{identifier}', [PostController::class, 'index'], ['GET', 'HEAD']),
-    new \PhpDevCommunity\Route('posts.show', '/post/{identifier}', [PostController::class, 'show'], ['GET', 'HEAD']),
-    new \PhpDevCommunity\Route('posts.comment', '/posts/{id}/comment', [PostController::class, 'storeComment'], ['POST']),
+    // Post listing and details
+    new \PhpDevCommunity\Route('posts.index',        '/posts',                    [PostController::class, 'index'],        ['GET', 'HEAD']),
+    new \PhpDevCommunity\Route('posts.show',         '/posts/{post}',             [PostController::class, 'show'],         ['GET', 'HEAD']),
+    new \PhpDevCommunity\Route('posts.show_legacy',  '/post/{post}',              [PostController::class, 'show'],         ['GET', 'HEAD']), // for backward compatibility
 
-    // ACTIONS
-    new \PhpDevCommunity\Route('posts.store', '/posts/create', [PostController::class, 'store'], ['POST']),
-    new \PhpDevCommunity\Route('posts.like', '/posts/{id}/like', [PostController::class, 'like'], ['POST']),
-    new \PhpDevCommunity\Route('posts.dislike', '/posts/{id}/dislike', [PostController::class, 'dislike'], ['POST']),
-    new \PhpDevCommunity\Route('posts.favorite', '/posts/{id}/favorite', [PostController::class, 'favorite'], ['POST']),
-    new \PhpDevCommunity\Route('posts.report', '/posts/{id}/report', [PostController::class, 'report'], ['POST']),
-    new \PhpDevCommunity\Route('posts.share', '/posts/{id}/share', [PostController::class, 'share'], ['GET']),
+    // Post create
+    new \PhpDevCommunity\Route('posts.create_form',  '/posts/create',             [PostController::class, 'createForm'],   ['GET']),
+    new \PhpDevCommunity\Route('posts.store',        '/posts',                    [PostController::class, 'store'],        ['POST']),
 
-    // USERS API: GET /users
-    new \PhpDevCommunity\Route('users.register_form_get', '/register', [UserController::class, 'registerForm'], ['GET','HEAD']),
-    new \PhpDevCommunity\Route('users.login_form_get', '/login', [UserController::class, 'loginForm'], ['GET', 'HEAD']),
-    new \PhpDevCommunity\Route('users', '/users', [UserController::class, 'index'], ['GET', 'HEAD']),
-    new \PhpDevCommunity\Route('users.logout', '/logout', [UserController::class, 'logout'], ['GET','HEAD']),
-    new \PhpDevCommunity\Route('users.show', '/user/{id}', [UserController::class, 'show'], ['GET', 'HEAD']),
-    new \PhpDevCommunity\Route('profile.show', '/profile', [UserController::class, 'profile'], ['GET', 'HEAD']),
-    new \PhpDevCommunity\Route('profile.update_put', '/profile/update', [UserController::class, 'updateProfile'], ['POST']),
-    new \PhpDevCommunity\Route('profile.update_get', '/profile/update', [UserController::class, 'getProfileForm'], ['GET', 'HEAD']),
+    // Post update/delete
+    new \PhpDevCommunity\Route('posts.edit_form',    '/posts/{post}/edit',        [PostController::class, 'editForm'],     ['GET']),
+    new \PhpDevCommunity\Route('posts.update',       '/posts/{post}',             [PostController::class, 'update'],       ['POST']),
+    new \PhpDevCommunity\Route('posts.delete',       '/posts/{post}',             [PostController::class, 'delete'],       ['DELETE', 'POST']),
 
-    new \PhpDevCommunity\Route('users.register_form_create', '/register', [UserController::class, 'register'], ['POST']),
-    new \PhpDevCommunity\Route('users.login_form_create', '/login', [UserController::class, 'login'], ['POST']),
-    new \PhpDevCommunity\Route('users.create', '/users/create', [UserController::class, 'create'], ['POST']),
-    new \PhpDevCommunity\Route('users.update', '/users/{id}/update', [UserController::class, 'update'], ['POST']),
-    new \PhpDevCommunity\Route('users.delete', '/users/{id}/delete', [UserController::class, 'delete'], ['POST']),
+    // Post actions
+    new \PhpDevCommunity\Route('posts.like',         '/posts/{post}/like',        [PostController::class, 'like'],         ['POST']),
+    new \PhpDevCommunity\Route('posts.dislike',      '/posts/{post}/dislike',     [PostController::class, 'dislike'],      ['POST']),
+    new \PhpDevCommunity\Route('posts.favorite',     '/posts/{post}/favorite',    [PostController::class, 'favorite'],     ['POST']),
+    new \PhpDevCommunity\Route('posts.report',       '/posts/{post}/report',      [PostController::class, 'report'],       ['POST']),
+    new \PhpDevCommunity\Route('posts.share',        '/posts/{post}/share',       [PostController::class, 'share'],        ['GET']),
+
+    // Comments
+    new \PhpDevCommunity\Route('posts.comments.store', '/posts/{post}/comments',  [PostController::class, 'storeComment'], ['POST']),
+
+    // Favorites
+    new \PhpDevCommunity\Route('posts.favorites',    '/posts/favorites',          [PostController::class, 'favoritesPage'], ['GET']),
+    new \PhpDevCommunity\Route('posts.favorites.show','/posts/favorites/{post}',  [PostController::class, 'favoritesPage'], ['GET', 'HEAD']),
+
+    // =============================
+    // USER & AUTH ROUTES
+    // =============================
+
+    // Registration & Login (forms and actions)
+    new \PhpDevCommunity\Route('auth.register.form', '/register',                 [UserController::class, 'registerForm'], ['GET','HEAD']),
+    new \PhpDevCommunity\Route('auth.register',      '/register',                 [UserController::class, 'register'],     ['POST']),
+    new \PhpDevCommunity\Route('auth.login.form',    '/login',                    [UserController::class, 'loginForm'],    ['GET', 'HEAD']),
+    new \PhpDevCommunity\Route('auth.login',         '/login',                    [UserController::class, 'login'],        ['POST']),
+    new \PhpDevCommunity\Route('auth.logout',        '/logout',                   [UserController::class, 'logout'],       ['GET','HEAD']),
+
+    // Users management
+    new \PhpDevCommunity\Route('users.index',        '/users',                    [UserController::class, 'index'],        ['GET', 'HEAD']),
+    new \PhpDevCommunity\Route('users.create',       '/users',                    [UserController::class, 'create'],       ['POST']),
+    new \PhpDevCommunity\Route('users.show',         '/users/{user}',             [UserController::class, 'show'],         ['GET', 'HEAD']),
+    new \PhpDevCommunity\Route('users.update',       '/users/{user}',             [UserController::class, 'update'],       ['POST', 'PUT']),
+    new \PhpDevCommunity\Route('users.delete',       '/users/{user}',             [UserController::class, 'delete'],       ['POST', 'DELETE']),
+
+    // Profile
+    new \PhpDevCommunity\Route('profile.show',       '/profile',                  [UserController::class, 'profile'],      ['GET', 'HEAD']),
+    new \PhpDevCommunity\Route('profile.edit_form',  '/profile/edit',             [UserController::class, 'getProfileForm'],['GET', 'HEAD']),
+    new \PhpDevCommunity\Route('profile.update',     '/profile',                  [UserController::class, 'updateProfile'],['POST']),
 ];
 
 return $routes;
